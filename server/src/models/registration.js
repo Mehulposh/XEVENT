4c87873b7d850db9ec6da00b5bcb6c5a8d0205b3
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-08-24 23:26:45 +0530

    Update server/src/models/registration.js

--- server/src/models/registration.js
+++ server/src/models/registration.js
@@ -9,7 +9,7 @@ const registrationSchema = new mongoose.Schema(
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: 'user',
       required: true,
     },
     status: {
